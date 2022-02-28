e5cc48d81bfa22a764593bca09fd6fa21d2cf555
Łukasz Strama <strama.lukasz54@gmail.com>
2022-02-28 19:40:00 +0100

    time wrap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Stage, Layer, Text, Circle } from 'react-konva';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+let history = [{x: 100, y: 100}];
+let historyStep = 0;
+
+class App extends Component {
+  state = {position: history[0]};
+
+  handleUndo = () => {
+    if (historyStep === 0) {
+      console.clear();
+      return;
+    }
+    historyStep -= 1;
+    const previous = history[historyStep];
+    this.setState({
+      position: previous
+    });
+    console.log(previous);
+  };
+
+  handleRedo = () => {
+    if (historyStep === history.length - 1) {
+      return;
+    }
+    historyStep += 1;
+    const next = history[historyStep];
+    this.setState({
+      position: next
+    });
+    console.log(next);
+  };
+
+  handleDragEnd = e => {
+    history = history.slice(0, historyStep + 1);
+    const pos = {
+      x: e.target.x(),
+      y: e.target.y()
+    };
+    history = history.concat([pos]);
+    historyStep += 1;
+    this.setState({
+      position: pos
+    });
+    console.log(pos);
+  };
+
+  //===================================
+
+  render() {
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="undo" onClick={this.handleUndo} />
+          <Text text="redo" x={40} onClick={this.handleRedo} />
+          <Text text={"x: " +this.state.position.x} x={80}/>
+          <Text text={"y: " +this.state.position.y} x={120}/>
+          <Text text={"step: " + historyStep} x={180}/>
+          <Text text={"history: " + JSON.stringify(history)} x={250}/>
+          <Circle
+            x={this.state.position.x}
+            y={this.state.position.y}
+            width={100}
+            height={100}
+            fill="red"
+            draggable
+            onDragEnd={this.handleDragEnd}
+          />
+        </Layer>
+      </Stage>
+    );
+  }
+}
+
+//===================================
+
+render(<App />, 
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
